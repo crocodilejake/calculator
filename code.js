81fc39770cd69552f7934d87e87015ea4d9ef522
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-11-18 15:09:50 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,18 @@
-function () {
+function makeCode () {
 var number1 =["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50];
 var number2 =;
 var sign;
 var i;
-var text1 ="";
+var text1 ="else if(number1 ==";
+var text2 ="&& sign =";
+var text3 ="&& number2 =";
+var text4 =") {answer =";
+var text5="}";
+
 for (i = 0, i < number1.length ) {
 
 }
 
 document.getElementById("answer").innerHTML 
-//if(number1 == 1 &&) {}
+//else if(number1 ==  && number2 == && sign =) {}
 }
