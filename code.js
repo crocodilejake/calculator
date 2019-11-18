f33e4af702085780d0fbe8303182b64f50ab194d
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-11-17 20:17:05 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 function () {
-var number1 = 
+var number1 =;
+var number2 =;
 var sign;
 var i;
 var text1 ="";
@@ -8,4 +9,5 @@ for (i = 0, i < number1.length ) {
 }
 
 document.getElementById("answer").innerHTML 
+//if(number1 == 1 &&) {}
 }
