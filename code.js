d91f64bdda86e3e754837cb70e7d3c8eea057474
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-11-18 20:55:29 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 function makeCode () {
 var number1 =["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50];
+var number1 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twentyOne", "twentyTwo", "twentyThree", "twentyFour", "twentyFive", "twentySix", "twentySeven", "twentyEight", "twentyNine", "thirty", "thirtyOne", "thirtyTwo", "thirtyThree", "thrityFour", "thirtyFive", "thirtySix", "thirtySeven", "thirtyEight", "thirtyNine", "fourty", "fourtyOne", "fourtyTwo", "fourtyThree", "fourtyFour", "fourtyFive", "fourtySix", "fourtySeven", "fourtyEight", "fourtyNine", "fifty"];
 var number2 =;
 var sign;
 var i;
@@ -8,7 +9,7 @@ var text2 ="&& sign =";
 var text3 ="&& number2 =";
 var text4 =") {answer =";
 var text5="}";
-
+for () {
 for (i = 0, i < number1.length, i++ ) {
 
 }
@@ -16,3 +17,4 @@ for (i = 0, i < number1.length, i++ ) {
 document.getElementById("answer").innerHTML 
 //else if(number1 ==  && number2 == && sign =) {}
 }
+              }
