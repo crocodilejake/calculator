c018ccfa18f1e1a3f32f5373c52c7137eabcc55c
crocodilejake <38299530+crocodilejake@users.noreply.github.com>
2019-11-25 15:09:18 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,45 @@
+function freeLabor () {
+var num1 = ;
+var num2 = ;
+var sign =["+", "-", "*", "/"];
+var i;
+var x;
+var y;
+var text1 ="else if(number1 ==";
+var text2 ="&& sign =";
+var text3 ="&& number2 =";
+var text4 =") {answer =";
+var text5="}";
+var code =;
+//Way more efficient way to do this, but screw that.
+for (i < 4, i++) {
+sign[i];
+for (x < 50, x++) {
+x + 1;
+for (y < 50, y++) {
+var answer = x*y;
+code += text1 + num1 + text2 + sign + text3 + num2 + text4 + answer + text5 +"<br>";
+}
+}
+  
+}
+document.getElementByID("answer").innerHTML = code;
+console.log(code)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function makeCode () {
   
 var realnumber1 =["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50];
@@ -27,3 +69,4 @@ console.log(code)
 //else if(number1 ==  && number2 == && sign =) {}
 }
               }
+*/
